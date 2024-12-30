836115db0554792571cbcc6988aa4adfb92e1305
yegor <zhora-ivanov-0303@mail.ru>
2024-12-30 22:44:12 +0700

    add colors

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,27 @@ const wchar_t SUITS[] = {L'\u2660', L'\u2665', L'\u2666', L'\u2663'};
 std::vector<card> deck;
 std::vector<card> bot_hand;
 std::vector<card> user_hand;
-card trump_card;
+card* trump_card;
+
+std::string set_color_mode(wchar_t suit) {
+    std::string color_code;
+
+    switch (suit) {
+        case L'♠':
+        case L'♣':
+            color_code = "\033[30m"; // Black
+            break;
+        case L'♥':
+        case L'♦':
+            color_code = "\033[31m"; // Red
+            break;
+        default:
+            color_code = "\033[0m";  // Default
+            break;
+    }
+
+    return color_code;
+}
 
 void make_deck() {
     for (const wchar_t& suit : SUITS) {
@@ -59,8 +79,10 @@ void show_hand(std::vector<card>& hand_to_show) {
             }
 
             std::string rank_on_card = hand_to_show[j].get_symbols();
+            std::string color_code = set_color_mode(hand_to_show[j].get_suit());
 
-            std::wcout << L"|  " << hand_to_show[j].get_suit() << rank_on_card[0] << rank_on_card[1] << L"  |";
+            std::wcout << L"|  " << color_code[0] << color_code[1] << color_code[2] << color_code[3] << color_code[4] 
+                << hand_to_show[j].get_suit() << L"\033[0m" << rank_on_card[0] << rank_on_card[1] << L"  |";
         }
         std::wcout << std::endl;
 
@@ -95,7 +117,7 @@ void show_hand(std::vector<card>& hand_to_show) {
 void sort_hand(std::vector<card>& hand_to_sort) {
     uint8_t trump_edge = 0;
     for (uint8_t i = 0; i < hand_to_sort.size(); i++) {
-        if (hand_to_sort[i].get_suit() != trump_card.get_suit()) {
+        if (hand_to_sort[i].get_suit() != trump_card->get_suit()) {
             continue;
         }
 
@@ -127,13 +149,15 @@ void sort_hand(std::vector<card>& hand_to_sort) {
 }
 
 void show_game_field() {
-    uint8_t deck_size = deck.size() + 1;
-    std::string trump_card_symbols = trump_card.get_symbols();
+    uint8_t deck_size = deck.size();
+    std::string trump_card_symbols = trump_card->get_symbols();
+    std::string color_code = set_color_mode(trump_card->get_suit());
 
     std::wcout << L"/¯¯¯¯¯¯¯\\\n";
     std::wcout << L"|       |¯¯¯¯\\\n";
     std::wcout << L"|   " << deck_size << ((deck_size > 9) ? L"  | " : L"   | ") 
-        << trump_card.get_suit() << trump_card_symbols[0] << trump_card_symbols[1] << L"|   "
+        << color_code[0] << color_code[1] << color_code[2] << color_code[3] << color_code[4]
+        << trump_card->get_suit() << L"\033[0m" << trump_card_symbols[0] << trump_card_symbols[1] << L"|   "
         << L"bot hand size: " << bot_hand.size() << std::endl;
     std::wcout << L"|       |____/\n";
     std::wcout << L"\\_______/\n";
@@ -143,9 +167,6 @@ int main() {
     srand(time(nullptr));
     std::locale::global(std::locale("en_US.UTF-8"));
     std::wcout.imbue(std::locale());
-
-    // card new_card(RANKS[rand() % 9], SUITS[rand() % 4]);
-    // new_card.show_card();
 
     make_deck();
     shuffle_deck();
@@ -158,19 +179,15 @@ int main() {
         // std::wcout << user_hand[i].get_rank() << user_hand[i].get_suit() << std::endl;
     }
 
-    trump_card = deck.back();
+    deck.insert(deck.begin(), deck.back());
     deck.pop_back();
+    trump_card = &deck[0];
 
     show_game_field();
     sort_hand(user_hand);
 
     // show_hand(bot_hand);
     show_hand(user_hand);
-
-    // while (!deck.empty()) {
-    //     deck.back().show_card();
-    //     deck.pop_back();
-    // }
 
     return 0;
 }
